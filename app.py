a5f65e6e88905556dbca264f9cdd986f4d8dce11
Кимосов Дмитрий <dmitry.it.develop@gmail.com>
2024-09-11 16:36:21 +0700

    Информация об авторе

--- app.py
+++ app.py
@@ -3,10 +3,28 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route('/web')
 def start():
-    return """<!doctype html>" 
+    return """<!doctype html>
         <html>
            <body>
                <h1>web-server na flask</h1>
+               <a href='/author'>author</a>
+            </body>
+        </html>"""
+
+@app.route('/author')
+def author():
+    name = 'Кимосов Дмитрий Владимирович'
+    group = 'ФБИ-21'
+    faculty = 'ФБ'
+
+    return f"""<!doctype html> 
+        <html>
+           <body>
+               <p>Студент: {name}</p>
+               <p>Группа: {group}</p>
+               <p>Факультет: {faculty}</p>
+               <a href='/web'>web</a>
             </body>
         </html>"""
