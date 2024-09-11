1145809cf1e189960fdbceceb9d1a0c3544d9572
Кимосов Дмитрий <dmitry.it.develop@gmail.com>
2024-09-11 17:16:52 +0700

    Исправление адресов

--- app.py
+++ app.py
@@ -3,17 +3,17 @@ from flask import Flask, url_for, redirect
 app = Flask(__name__)
 
 @app.route("/")
-@app.route('/web')
+@app.route('/lab1/web')
 def start():
     return """<!doctype html>
         <html>
            <body>
                <h1>web-server na flask</h1>
-               <a href='/author'>author</a>
+               <a href=''/lab1/author'>author</a>
             </body>
         </html>""", 200, {'X-Server':'sample','Content-Type': 'text/plain; charset=utf-8'}
 
-@app.route('/author')
+@app.route('/lab1/author')
 def author():
     name = 'Кимосов Дмитрий Владимирович'
     group = 'ФБИ-21'
@@ -25,7 +25,7 @@ def author():
                <p>Студент: {name}</p>
                <p>Группа: {group}</p>
                <p>Факультет: {faculty}</p>
-               <a href='/web'>web</a>
+               <a href='/lab1/web'>web</a>
             </body>
         </html>"""
 
@@ -59,9 +59,9 @@ def counter():
             </body>
         </html>"""
 
-@app.route('/info')
+@app.route('/lab1/info')
 def info():
-    return redirect('/author')
+    return redirect('/lab1/author')
 
 @app.route('/lab1/created') 
 def created():
