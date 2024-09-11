646d4b6ecf612e30bd3c3ee5a88cb0b5afbc0d27
Кимосов Дмитрий <dmitry.it.develop@gmail.com>
2024-09-11 16:29:51 +0700

    tri kovichki

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "<!doctype html>" \
-        "<html>"\
-        "   <body>"\
-        "       <h1>web-server na flask</h1>"\
-        "   </body>"\
-        "</html>"
+    return """<!doctype html>" 
+        <html>
+           <body>
+               <h1>web-server na flask</h1>
+            </body>
+        </html>"""
