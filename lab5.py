03efc68a45fe5902804df6fad65c46ef90586303
Кимосов Дмитрий <dmitry.it.develop@gmail.com>
2024-11-20 15:03:32 +0700

    Самостоятельное задание

--- lab5.py
+++ lab5.py
@@ -40,6 +40,7 @@ def lab5_registe():
         cur.execute(f"INSERT INTO users (login, password) VALUES (%s, %s);", (login, password_hash))
     else:
         cur.execute(f"INSERT INTO users (login, password) VALUES (?, ?);", (login, password_hash))
+    session['login'] = login
     db_close(conn,cur)
     return render_template('lab5/success.html', login=login)
 
@@ -77,6 +78,12 @@ def lab5_login():
     session['login'] = login
     db_close(conn, cur)
     return render_template('lab5/login.html', login=login, authorized=True)
+
+@lab5.route('/lab5/quit')
+def lab5_quit():
+    session.pop('login', None)
+    return redirect('/lab5')
+
    
 @lab5.route('/lab5/create', methods=['GET', 'POST'])
 def lab5_create():
@@ -84,11 +91,17 @@ def lab5_create():
         return redirect('/lab5/login')
     if request.method == 'GET':
         return render_template('lab5/create.html')   
-    
+    errors = {}
     login = session['login']
     title = request.form.get('title')
     article_text = request.form.get('article_text')
 
+    if title == '' or article_text == '':
+        if title == '':
+            errors['title'] = 'Введите значение!'
+        if article_text == '':
+            errors['article_text'] = 'Введите значение!'
+        return render_template('lab5/create.html', errors=errors, title=title)
     conn, cur = db_connect()
     if current_app.config['DB_TYPE'] == 'postgres':
         cur.execute(f"SELECT * FROM users WHERE login=%s;", (login, ))
@@ -114,14 +127,62 @@ def lab5_list():
     cur.execute(f"SELECT * FROM users WHERE login='{session.get('login')}'")
     user_id = cur.fetchone()['id']
     if current_app.config['DB_TYPE'] == 'postgres':
-        cur.execute(f"SELECT * FROM articles WHERE user_id=%s", (user_id, ))
+        cur.execute(f"SELECT * FROM articles WHERE user_id=%s ORDER BY id", (user_id, ))
     else: 
-        cur.execute(f"SELECT * FROM articles WHERE user_id=?", (user_id, ))
+        cur.execute(f"SELECT * FROM articles WHERE user_id=? ORDER BY id", (user_id, ))
     articles = cur.fetchall()
 
     db_close(conn, cur)
 
     return render_template('/lab5/articles.html', articles=articles)
+
+@lab5.route('/lab5/delete_article', methods=['GET'])
+def lab5_delete_article():
+    if not session:
+        redirect('/lab5')
+    id = request.args.get('id')
+    conn, cur = db_connect()
+    if current_app.config['DB_TYPE'] == 'postgres':
+        cur.execute(f"DELETE FROM articles WHERE id=%s", (id, ))
+    else:
+        cur.execute(f"DELETE FROM articles WHERE id=?", (id, ))
+    db_close(conn, cur)
+    return redirect('/lab5/list')
+
+@lab5.route('/lab5/edit_article', methods=['GET', 'POST'])
+def lab5_edit_article():
+    if not session:
+        redirect('/lab5')
+
+    id = request.args.get('id')
+    conn, cur = db_connect()
+
+    if current_app.config['DB_TYPE'] == 'postgres':
+        cur.execute(f"SELECT * FROM articles WHERE id=%s", (id, ))
+    else:
+        cur.execute(f"SELECT * FROM articles WHERE id=?", (id, ))
+
+    article = cur.fetchone()
+    if request.method == 'GET':
+        return render_template('lab5/edit.html', article=article)
+
+    login = session['login']
+    title = request.form.get('title')
+    article_text = request.form.get('article_text')
+
+    if title == '' or article_text == '':
+        if title == '':
+            errors['title'] = 'Введите значение!'
+        if article_text == '':
+            errors['article_text'] = 'Введите значение!'
+        return render_template('lab5/create.html', errors=errors, title=title)
+    
+    if current_app.config['DB_TYPE'] == 'postgres':
+        cur.execute(f"UPDATE articles SET title=%s, article_text=%s WHERE id=%s;", (title, article_text, id))
+    else:
+        cur.execute(f"UPDATE articles SET title=?, article_text=? WHERE id=?;", (title, article_text, id))
+    db_close(conn, cur)
+    return redirect('/lab5/list')
 
 
 def db_connect():
