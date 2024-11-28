dc456f1972645879977fa255aa5046d350c6396b
Кимосов Дмитрий <dmitry.it.develop@gmail.com>
2024-11-28 12:20:07 +0700

    Бронирование офисов

--- lab6.py
+++ lab6.py
@@ -9,3 +9,58 @@ lab6 = Blueprint('lab6', __name__)
 @lab6.route('/lab6')
 def lab6_main():
     return render_template('lab6/lab6.html', login=session.get('login'))
+
+offices = []
+
+for i in range(1,11):
+    offices.append({'number':i, 'tenant':""})
+
+@lab6.route('/lab6/json-rpc-api/', methods=['POST'])
+def lab6_json_rpc_api():
+    data = request.json
+    id = data['id']
+    if data['method'] == 'info':
+        return {
+            'jsonrpc': 2.0,
+            'result': offices,
+            'id': id
+        }
+    login = session.get('login')
+    if not login:
+        return {
+            'jsonrpc': 2.0,
+            'error': {
+                'code': 1,
+                'message': 'Unauthorized'
+            }
+        }
+    if data['method'] == 'booking':
+        office_number = data['params']
+        for office in offices:
+            if office['number'] == office_number:
+                if office['tenant'] != '':
+                    return {
+                        'jsonrpc': 2.0,
+                        'error': {
+                            'code': 2,
+                            'message': 'Already booked'
+                        }
+                    }
+                office['tenant'] = login
+                return {
+                    'jsonrpc': 2.0,
+                    'result': 'succes',
+                    'id': id
+                }
+    else:
+        return {
+            'jsonrpc': 2.0,
+            'error': {
+                'code': -32601,
+                'message': 'Method not found'
+            }
+        }
+
+@lab6.route('/lab6/office')
+def lab6_office():
+    return render_template('lab6/office.html')
